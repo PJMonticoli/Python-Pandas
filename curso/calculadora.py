c575548cc63fa63eb088862adb55cfa6354d60fa
PJMonticoli <pablo_monticoli@hotmail.com>
2024-06-12 11:11:58 -0300

    calculadora

---
+++ curso/calculadora.py
@@ -0,0 +1,15 @@
+n1 = int(input("Ingrese el primer numero "))
+n2 = int(input("Ingrese el segundo numero "))
+
+suma = n1 + n2
+resta = n1 - n2
+division = n1 / n2
+multiplicacion = n1 * n2
+
+
+print("--------------------------------------")
+print(f"Primer Nro: {n1} y Segundo Nro: {n2}")
+print(f"El resultado de la suma es de {suma}")
+print(f"El resultado de la resta es de {resta}")
+print(f"El resultado de la division es de {division}")
+print(f"El resultado de la multiplicacion es de {multiplicacion}")
