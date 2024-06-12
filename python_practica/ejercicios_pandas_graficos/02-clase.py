24a48428e43e1e90eee556ca12f5e49368ca656e
PJMonticoli <pablo_monticoli@hotmail.com>
2024-06-12 12:08:18 -0300

    creando dataframes de ejemplo

---
+++ python_practica/ejercicios_pandas_graficos/02-clase.py
@@ -0,0 +1,20 @@
+# import pandas as pd
+
+
+# crear un dataframe desde un diccionario
+
+# datos = {
+#     "Nombre": ["Pablo", "Eduardo", "Juan", "Laura"],
+#     "Edad": [24, 59, 23, 56]
+# }
+
+
+# df = pd.DataFrame(datos)
+# print(df)
+
+# Otra manera de crear un dataframe
+# datos2 = [{'Nombre': 'Pablo', 'Edad': 24},
+#           {'Nombre': 'Juan', 'Edad': 24}
+#           ]
+# df2 = pd.DataFrame(datos2)
+# print(df2)
